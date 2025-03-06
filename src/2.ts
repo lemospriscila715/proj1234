69e501638e0836d430bc4ac45259bbb02bb3544c
lemospriscila715 <lemospriscila715@gmail.com>
2025-03-06 19:22:14 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,9 @@
+const generateRandomTsCode = () => {
+  const tsCode = 'type Something = number | string;';
+
+  return tsCode;
+}
+
+module.exports = {
+  generateRandomTsCode: generateRandomTsCode,
+};

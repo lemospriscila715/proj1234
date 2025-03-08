bd0308b7e9739cd68e20899ddf4499b918bf7968
lemospriscila715 <lemospriscila715@gmail.com>
2025-03-08 15:11:33 +0000

    Add 4.ts

---
+++ src/4.ts
@@ -0,0 +1,11 @@
+function getRandomTSCode(): string {
+  const code = `
+    import { Component } from '@angular/core';
+    @Component({
+      selector: 'app-root',
+      template: '<h1>Hello World!</h1>'
+    })
+    export class AppComponent {}
+  `;
+  return code;
+}

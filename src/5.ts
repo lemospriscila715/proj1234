9c35dc05782819e179c88a1fba69d3b490740309
lemospriscila715 <lemospriscila715@gmail.com>
2025-03-09 10:49:48 +0000

    Add 5.ts

---
+++ src/5.ts
@@ -0,0 +1,10 @@
+const proj1234 = {
+  name: 'proj1234',
+  description: 'A simple school project for my class',
+  setup: () => {
+    console.log('setting up proj1234');
+  },
+  run: () => {
+    console.log('running proj1234');
+  },
+};
